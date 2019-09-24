2afd3ed77da5341b269d08377bda9ae5903dd020
swarupd <swarupd@zillowgroup.com>
2019-09-24 10:46:02 -0700

    fixed case

--- Formula/superclone.rb
+++ Formula/superclone.rb
@@ -1,4 +1,4 @@
-class SuperClone < Formula
+class Superclone < Formula
   desc "Tool to clone git repositories using browser http/https urls"
   homepage "https://github.com/swarupdonepudi/super-clone"
   url "https://github.com/swarupdonepudi/super-clone/releases/download/0.1/clone"
