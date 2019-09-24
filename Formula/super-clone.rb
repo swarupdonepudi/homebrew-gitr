15de511a8612bbd13a2839a628018a602d2428de
swarupd <swarupd@zillowgroup.com>
2019-09-24 10:43:13 -0700

    added a simple step to install the binary

--- Formula/super-clone.rb
+++ Formula/super-clone.rb
@@ -1,10 +1,9 @@
 class SuperClone < Formula
   desc "Tool to clone git repositories using browser http/https urls"
   homepage "https://github.com/swarupdonepudi/super-clone"
-  url "https://github.com/swarupdonepudi/super-clone/releases/download/0.1/clone-darwin"
+  url "https://github.com/swarupdonepudi/super-clone/releases/download/0.1/clone"
 
   def install
-    system "sudo", "mv", "clone-darwin","/usr/local/bin/clone"
-    bin.install_symlink Dir["#{libexec}/bin/*"]
+    bin.install "clone-darwin"
   end
 end
