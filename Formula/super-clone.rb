ffcac1cc336bf3bb554d5e6a361371aad651f79a
swarupdonepudi <swarup.donepudi@gmail.com>
2019-09-23 22:42:23 -0700

    changed broken formula

--- Formula/super-clone.rb
+++ Formula/super-clone.rb
@@ -4,6 +4,7 @@ class SuperClone < Formula
   url "https://github.com/swarupdonepudi/super-clone/releases/download/0.1/clone-darwin"
 
   def install
-    mv clone-darwin /usr/local/bin/clone
+    system "mv", "clone-darwin","/usr/local/bin/clone"
+    bin.install_symlink Dir["#{libexec}/bin/*"]
   end
 end
