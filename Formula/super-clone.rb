5c50411d6374f20e5c9af6e55e0cc66f0768b5e3
swarupdonepudi <swarup.donepudi@gmail.com>
2019-09-23 22:31:51 -0700

    added formula file

---
+++ Formula/super-clone.rb
@@ -0,0 +1,9 @@
+class SuperClone < Formula
+  desc "Tool to clone git repositories using browser http/https urls"
+  homepage "https://github.com/swarupdonepudi/super-clone"
+  url "https://github.com/swarupdonepudi/super-clone/releases/download/0.1/clone-darwin"
+
+  def install
+    system "echo", "swaroop"
+  end
+end
