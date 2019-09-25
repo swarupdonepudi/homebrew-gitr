abc86f8f556d64c14f1aa80a1552fe5a1d2e4aee
swarupdonepudi <swarup.donepudi@gmail.com>
2019-09-25 14:04:03 -0700

    changed binary and sha256 for the binary

--- Formula/gitr.rb
+++ Formula/gitr.rb
@@ -7,7 +7,7 @@ class Gitr < Formula
   head "https://github.com/swarupdonepudi/gitr.git",
     :shallow => false
   version "v1.0"
-  sha256 "c1075c3e17bf7be22583f403522a43b21b941e4f57e553b45e46f0fc93a12f5d"
+  sha256 "8f604cc8fa46cf8886bf79190d60050e73323a2ea85e5b6ac41298520c497103"
   def install
     bin.install "gitr"
   end
