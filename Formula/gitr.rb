fc3fee7c98cb62b7492343a684bbf53cd32e472a
swarupd <swarupd@zillowgroup.com>
2019-09-24 19:58:44 -0700

    updated to 0.4 which has git rem command

--- Formula/gitr.rb
+++ Formula/gitr.rb
@@ -1,13 +1,13 @@
 class Gitr < Formula
   desc "Tool to clone git repositories using browser http/https urls"
   homepage "https://github.com/swarupdonepudi/gitr"
-  url "https://github.com/swarupdonepudi/super-clone/releases/download/0.3/gitr",
-    :tag      => "v0.3",
-    :revision => "5bc4ea6e8675435589f4141f6f5156f4b30a04ea"
+  url "https://github.com/swarupdonepudi/super-clone/releases/download/0.4/gitr",
+    :tag      => "v0.4",
+    :revision => "8ec15b61e8608a454791d7e84b21c973833d95de"
   head "https://github.com/swarupdonepudi/gitr.git",
     :shallow => false
-  version "0.3"
-  sha256 "4fb6643cd48e53759aaeae2bb0890d25f2fbfcff894012ed3360b54a60fe45c0"
+  version "0.4"
+  sha256 "abbe92cbb9e5521ec688e9b091c85f84d32da28eef7231bef7150207fea2b64d"
   def install
     bin.install "gitr"
   end
