6a8f02f9f9496a1da35940a75a3dc9be61048f0c
swarupdonepudi <swarup.donepudi@gmail.com>
2019-09-25 22:19:20 -0700

    updated the decription and bumped up to v1.1 that has support for bitbucket dc edition and config file and debug flag

--- Formula/gitr.rb
+++ Formula/gitr.rb
@@ -1,13 +1,13 @@
 class Gitr < Formula
-  desc "Tool to clone git repositories using browser http/https urls"
+  desc "Git Rapid: The missing link between Git CLI and SCM Providers"
   homepage "https://github.com/swarupdonepudi/gitr"
-  url "https://github.com/swarupdonepudi/gitr/releases/download/v1.0/gitr",
-    :tag      => "v1.0",
-    :revision => "931f70bc2338cebcb61127ed3f2192020b62bbbb"
+  url "https://github.com/swarupdonepudi/gitr/releases/download/v1.1/gitr",
+    :tag      => "v1.1",
+    :revision => "53aaac279a2bff0a78a798622059fc689e501296"
   head "https://github.com/swarupdonepudi/gitr.git",
     :shallow => false
-  version "v1.0"
-  sha256 "9e3f5ca945b80bf60fe2df1feee27acb997c84a22daaac41cfc0b6d1576c7eca"
+  version "v1.1"
+  sha256 "222ff9dad0753e5d6f10708583dd414da50a3d851ec73ecb056b0093bf4db785"
   def install
     bin.install "gitr"
   end
