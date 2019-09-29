e6db9440560f0bbfa3cb9766c0f3a93f19a6c290
swarupdonepudi <swarup.donepudi@gmail.com>
2019-09-29 12:17:18 -0700

    updating to v1.1.2 which has bugfix for clone null pointer

--- Formula/gitr.rb
+++ Formula/gitr.rb
@@ -1,13 +1,13 @@
 class Gitr < Formula
   desc "GitRapid: The missing link between Git CLI and SCM Providers"
   homepage "https://github.com/swarupdonepudi/gitr"
-  url "https://github.com/swarupdonepudi/gitr/releases/download/v1.1.1/gitr",
-    :tag      => "v1.1.1",
-    :revision => "a5218fe495bf3c61c06f7c06994c490dd0a465e2"
+  url "https://github.com/swarupdonepudi/gitr/releases/download/v1.1.2/gitr",
+    :tag      => "v1.1.2",
+    :revision => "810f341ed63af95cb06a7f2a4475e8240dbf070a"
   head "https://github.com/swarupdonepudi/gitr.git",
     :shallow => false
-  version "v1.1.1"
-  sha256 "9a4967775e0ba6761de485f0efac16b278db8f74e3fd335764b43b740c58e998"
+  version "v1.1.2"
+  sha256 "92a79fc807756f0899bf5533397fc418a4a6f1c625599359b5fc53dc1797a22c"
   def install
     bin.install "gitr"
   end
