9f22fd95a0c26d0f524c687b4cc1a0f34b33a563
swarupdonepudi <swarup.donepudi@gmail.com>
2019-09-28 19:40:43 -0700

    updated to v1.1.1 which has bugfix for https remote urls with username in em

--- Formula/gitr.rb
+++ Formula/gitr.rb
@@ -1,13 +1,13 @@
 class Gitr < Formula
-  desc "Git Rapid: The missing link between Git CLI and SCM Providers"
+  desc "GitRapid: The missing link between Git CLI and SCM Providers"
   homepage "https://github.com/swarupdonepudi/gitr"
-  url "https://github.com/swarupdonepudi/gitr/releases/download/v1.1/gitr",
-    :tag      => "v1.1",
-    :revision => "53aaac279a2bff0a78a798622059fc689e501296"
+  url "https://github.com/swarupdonepudi/gitr/releases/download/v1.1.1/gitr",
+    :tag      => "v1.1.1",
+    :revision => "a5218fe495bf3c61c06f7c06994c490dd0a465e2"
   head "https://github.com/swarupdonepudi/gitr.git",
     :shallow => false
-  version "v1.1"
-  sha256 "77f446ebf2298c6b3e7638ea6cbc14fff013369b2db18d5e69ed4f322045e82d"
+  version "v1.1.1"
+  sha256 "9a4967775e0ba6761de485f0efac16b278db8f74e3fd335764b43b740c58e998"
   def install
     bin.install "gitr"
   end
