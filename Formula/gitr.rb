47d0356ae3120b988e2b1b7230369ec8236c96bc
Swarup Donepudi <swarup.donepudi@gmail.com>
2020-02-24 18:38:07 -0800

    updated checksums

--- Formula/gitr.rb
+++ Formula/gitr.rb
@@ -3,11 +3,11 @@ class Gitr < Formula
   homepage "https://github.com/swarupdonepudi/gitr"
   url "https://github.com/swarupdonepudi/gitr/releases/download/v1.1.4/gitr-darwin",
     :tag      => "v1.1.4",
-    :revision => "cc0fd5e1f71e76a8f74566824a0c2bad4b34cb15"
+    :revision => "06382a17cd10c0098ac25a747641a2c96b10ff0e"
   head "https://github.com/swarupdonepudi/gitr.git",
     :shallow => false
   version "v1.1.3"
-  sha256 "7b91edbaba299509bacbc3296c53a6ef635d697ddcdf93f356df45570fc468ac"
+  sha256 "f31aa33d3f8d19ad13ecc72dcab6dd4a57a73ccea7c64afde652ea514f62f9ba"
   def install
     bin.install "gitr"
   end
