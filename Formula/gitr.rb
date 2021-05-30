fd675df2bd58b1594a5d47689da09aabde878dbe
swarupdonepudi <swarup.donepudi@gmail.com>
2021-05-30 03:56:30 -0500

    updated to 2.1.0

--- Formula/gitr.rb
+++ Formula/gitr.rb
@@ -1,13 +1,13 @@
 class Gitr < Formula
-  desc "GitRapid: The missing link between Git CLI and SCM Providers"
+  desc "git-rapid: save time(a ton) by opening git repos on web browser right from the command line"
   homepage "https://github.com/swarupdonepudi/gitr"
-  url "https://github.com/swarupdonepudi/gitr/releases/download/v2.0.0/gitr-darwin",
-    :tag      => "v2.0.0",
-    :revision => "319fad1dd5660bee4850b1a13b496ad6d1de43a4"
+  url "https://github.com/swarupdonepudi/gitr/releases/download/v2.1.0/gitr-darwin",
+    :tag      => "v2.1.0",
+    :revision => "128cdc000d3add4c313d3afa52fe84f7112e18af"
   head "https://github.com/swarupdonepudi/gitr.git",
     :shallow => false
-  version "v2.0.0"
-  sha256 "92ea94884f7fb7f1c870d3ba4df596d1ed7bb2e666d5e2abe53b0b4f07a05327"
+  version "v2.1.0"
+  sha256 "0435f8bbfafefe17040707767bf0573b76ac36e8b3509ff3569e70cfe6bdaa42"
   def install
     bin.install "gitr-darwin"
     mv bin/"gitr-darwin", bin/"gitr"
