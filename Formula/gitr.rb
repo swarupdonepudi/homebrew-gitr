60a9c9d9e835c417e879e7999f89b5de1936f49e
swarupdonepudi <swarup.donepudi@gmail.com>
2021-06-02 14:27:58 -0500

    2.1.2

--- Formula/gitr.rb
+++ Formula/gitr.rb
@@ -1,13 +1,13 @@
 class Gitr < Formula
   desc "git-rapid: save time(a ton) by opening git repos on web browser right from the command line"
   homepage "https://github.com/swarupdonepudi/gitr"
-  url "https://github.com/swarupdonepudi/gitr/releases/download/v2.1.1/gitr-darwin",
-    :tag      => "v2.1.1",
-    :revision => "98b865a055b3bf66cc018a8d54aa5afe90f70cdf"
+  url "https://github.com/swarupdonepudi/gitr/releases/download/v2.1.2/gitr-darwin",
+    :tag      => "v2.1.2",
+    :revision => "5a73bf0f67e695fcc201f56381d6e8599ce96626"
   head "https://github.com/swarupdonepudi/gitr.git",
     :shallow => false
-  version "v2.1.1"
-  sha256 "d0a579894213cdc84ef028e95b3006719bbd2708efd23fecfb481e82df1cc3c3"
+  version "v2.1.2"
+  sha256 "25084e1cbb1e39051ab3b5f3ce320d5d85e1a720b0ea4ffe571aaadcc210c32a"
   def install
     bin.install "gitr-darwin"
     mv bin/"gitr-darwin", bin/"gitr"
